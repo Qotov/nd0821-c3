29546bb64c2ca310214749686b50d86a2ae45d0f
Qotov <49810237+Qotov@users.noreply.github.com>
2021-12-23 20:40:51 +0100

    Update train_model.py

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -2,9 +2,17 @@
 
 from sklearn.model_selection import train_test_split
 
-# Add the necessary imports for the starter code.
+import pandas as pd
+from ml.data import process_data
+from ml.model import train_model, compute_model_metrics, inference, save_model
+
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
 
 # Add code to load in the data.
+data = pd.read_csv("./data/census_clean.csv")
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
@@ -19,10 +27,9 @@ cat_features = [
     "sex",
     "native-country",
 ]
+
 X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
 
-# Proces the test data with the process_data function.
 
-# Train and save a model.
